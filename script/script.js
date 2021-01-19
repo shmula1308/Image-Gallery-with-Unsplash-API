eec10725ad36f21d4f81872b8867aa95f14ccc88
Shpend Mula <shmula@hotmail.com>
2021-01-19 17:00:10 +0100

    created modal gallery

--- script/script.js
+++ script/script.js
@@ -1,6 +1,11 @@
-const imagesContainer = document.querySelector('.container')
+const imagesContainer = document.querySelector('.container');
+const modGallery = document.querySelector('.modal-gallery');
+const modImage = document.querySelector('.modal-img img');
+const arrows = document.querySelectorAll('.modal-arrows');
+const closeBtn = document.querySelector('.closeBtn')
 let images = [];
-
+let allImages = [];
+let index = 0;
 document.addEventListener('DOMContentLoaded', init);
 
 
@@ -25,6 +30,7 @@ function init() {
 function displayImages(data) {
     let df = new DocumentFragment();
     images = data.slice(0,5);
+    allImages = data;
 
     // Create images
     images.forEach((image,idx) => {
@@ -32,23 +38,80 @@ function displayImages(data) {
         div.classList.add('img' + Number(idx+1));
         let img = document.createElement('img');
         img.src = image.urls.regular;
-        img.alt = image.alt_description
+        img.alt = image.alt_description;
+        img.setAttribute("data-index",idx);
         div.append(img)
         df.append(div)
     })
     imagesContainer.append(df);
     
-    //calculate remianing number of images in gallery
+    //calculate remianing number of images in the gallery
     let div = document.createElement('div');
     div.classList.add('number');
     let totalNumber = data.length - 5;
     div.textContent = "+" + totalNumber;
     imagesContainer.lastElementChild.append(div);
 
-    let img = imagesContainer.querySelectorAll('img');
-    
+    let displayedImgs = imagesContainer.querySelectorAll('img');
+    displayedImgs.forEach(image => {
+        image.addEventListener('click', modalGallery)
+    }) 
 }
 
+
+function modalGallery(ev) {
+   imagesContainer.style.display = 'none';
+   modGallery.style.display = "block";
+   modImage.src = ev.target.src;
+   index = ev.target.getAttribute('data-index');
+   console.log(index)
+   if(index === '0') {
+    document.getElementById('left').style.display = "none"
+   }
+   
+}
+
+arrows.forEach(arrow => {
+    arrow.addEventListener('click', changeImage);
+})
+
+function changeImage(ev) {
+    if(ev.target.id === 'right') {
+        index++;
+        arrowsDisplay(index);
+        let src = allImages[index].urls.regular;
+        modImage.src = src;
+    } else {
+        index--;
+        arrowsDisplay(index);
+        let src = allImages[index].urls.regular;
+        modImage.src = src;
+    }  
+}
+
+function arrowsDisplay(idx) {
+    console.log(idx)
+    if(idx === 0) {
+        document.getElementById('left').style.display = 'none';
+        index = 0;
+        return;
+    } else {
+        document.getElementById('left').style.display = 'block';
+    }
+    if(idx === allImages.length - 1) {
+        index = allImages.length - 1;
+        document.getElementById('right').style.display = 'none';
+    }else {
+        document.getElementById('right').style.display = 'block';
+    }
+}
+
+
+closeBtn.addEventListener('click', () => {
+    modGallery.style.display = 'none';
+    imagesContainer.style.display = 'grid';
+    
+} )
 
 
 
